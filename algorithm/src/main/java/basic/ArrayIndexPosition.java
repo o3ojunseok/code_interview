30925745853194b42ec63910cc4d064d9f5392e1
o3ojunseok <o3ojunseok@gmail.com>
2024-05-03 11:55:22 +0900

    Feat: ArrayIndexPosition

---
+++ algorithm/src/main/java/basic/ArrayIndexPosition.java
@@ -0,0 +1,21 @@
+package basic;
+
+ // main 메소드에 최소 10개 이상의 요소로 구성된 integer array를 정의 한 후 해당 array의 최소 값이 위치한 자리수와 그 값을 출력한다.
+
+public class ArrayIndexPosition {
+    public static void main(String[] args) {
+        int[] a = new int[]{12, 44, 23, 56, 9, 23, 78, 13};
+
+        int min = a[0];
+        int index = 0;
+
+        for (int i = 1; i < a.length; i++) {
+            if (min > a[i]) {
+                min = a[i];
+                index = i;
+            }
+        }
+        System.out.println("index: " + index);
+        System.out.println("min:" + min);
+    }
+}
