70818e12d928990f71198325756602f2dad0f9bd
o3ojunseok <o3ojunseok@gmail.com>
2024-05-11 02:42:39 +0900

    Feat: PriorityQueueExample

---
+++ algorithm/src/main/java/basic/collection/structure/PriorityQueueExample.java
@@ -0,0 +1,34 @@
+package basic.collection.structure;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueExample {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        // enqueue
+        priorityQueue.add(3);
+        priorityQueue.add(1);
+        priorityQueue.add(2);
+        // dequeue
+        int element = priorityQueue.poll();
+
+        System.out.println(element); // 출력 1 : 우선순위가 가장 높은 값이 먼저 꺼내짐
+        priorityQueue.peek();
+        printQueue(priorityQueue);
+
+        priorityQueue.add(5);
+        priorityQueue.add(4);
+        priorityQueue.add(3);
+
+        if (!priorityQueue.isEmpty()) {
+            System.out.println(priorityQueue.poll());
+            printQueue(priorityQueue);
+        }
+    }
+
+    private static void printQueue(PriorityQueue<Integer> priorityQueue) {
+        while (!priorityQueue.isEmpty()) {
+            System.out.println("priorityQueue : " + priorityQueue.poll());
+        }
+    }
+}
