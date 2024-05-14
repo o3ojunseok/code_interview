b3eb6dac12e73df7ad70e132b3a8b0cdd6539f1d
o3ojunseok <o3ojunseok@gmail.com>
2024-05-14 13:50:03 +0900

    Feat: ListSort

---
+++ algorithm/src/main/java/basic/collection/sort/ListSort.java
@@ -0,0 +1,26 @@
+package basic.collection.sort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ListSort {
+
+    public static void main(String args[]) {
+        List<String> listObject = new ArrayList<String>();
+        listObject.add("z");
+        listObject.add("a");
+        listObject.add("f");
+        listObject.add("c");
+
+        System.out.println("##기본출력 : " + listObject);
+        // 문자열순으로 정렬
+//        Collections.sort(listObject, String.CASE_INSENSITIVE_ORDER);
+        listObject.sort(String.CASE_INSENSITIVE_ORDER);
+        System.out.println("##순차정렬 : " + listObject);
+        // 역순으로 정렬
+//        Collections.sort(listObject, Collections.reverseOrder());
+        listObject.sort(Collections.reverseOrder());
+        System.out.println("##역순정렬 : " + listObject);
+    }
+}
