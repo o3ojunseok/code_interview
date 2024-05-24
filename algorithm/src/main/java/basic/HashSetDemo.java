b8d88fa7d4dc835fc280ad7c598ee80418204e9e
o3ojunseok <o3ojunseok@gmail.com>
2024-05-24 11:19:54 +0900

    Feat: HashSetDemo

---
+++ algorithm/src/main/java/basic/HashSetDemo.java
@@ -0,0 +1,22 @@
+package basic;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetDemo {
+
+    public static void main(String args[]) {
+        // 해시 집합을 생성한다.
+        Set<String> hs = new HashSet<>();
+        // 해시 집합에 요소를 추가한다.
+        hs.add("B");
+        hs.add("A");
+        hs.add("D");
+        hs.add("E");
+        hs.add("C");
+        hs.add("F");
+        hs.toString();
+
+        System.out.println(hs);
+    }
+}
