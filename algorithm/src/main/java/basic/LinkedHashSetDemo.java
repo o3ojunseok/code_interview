c8633bac81c3f67cf0fb397bddfd1515bf0e1512
o3ojunseok <o3ojunseok@gmail.com>
2024-05-26 21:26:37 +0900

    Feat: LinkedHashSetDemo

---
+++ algorithm/src/main/java/basic/LinkedHashSetDemo.java
@@ -0,0 +1,18 @@
+package basic;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+
+class LinkedHashSetDemo {
+    public static void main(String[] args) {
+        Set<String> linkHashSet = new LinkedHashSet<String>();
+        linkHashSet.add("B");
+        linkHashSet.add("A");
+        linkHashSet.add("D");
+        linkHashSet.add("E");
+        linkHashSet.add("C");
+        linkHashSet.add("F");
+        System.out.println(linkHashSet);
+    }
+}
