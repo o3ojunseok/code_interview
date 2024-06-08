cdbbd5b6f6e2409fcb3e57654d24c3fe115f3169
o3ojunseok <o3ojunseok@gmail.com>
2024-06-08 16:58:19 +0900

    Feat: ReverseString

--- algorithm/src/main/java/basic/quiz/ReverseString.java
+++ algorithm/src/main/java/basic/quiz/ReverseString.java
@@ -1,19 +1,9 @@
 package basic.quiz;
 
 public class ReverseString {
-//    public static void main(String[] args) {
-//        String str = "ABCDE";
-//        char chars[] = str.toCharArray();
-//        for (int i = chars.length - 1; i >= 0; i--) {
-//            System.out.print(chars[i]);
-//        }
-//    }
-
-// 함수 활용
     public static void main(String[] args) {
         ReverseString rs = new ReverseString();
-        System.out.println(rs.reverseStr("ABCDE"));
-
+        System.out.println(rs.reverseStr("ABCDefg"));
     }
 
     private String reverseStr(String str) {
