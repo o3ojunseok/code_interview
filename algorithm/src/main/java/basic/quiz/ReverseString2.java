3434ab9ef327a0ec265c26077888bf134f9e61f9
o3ojunseok <o3ojunseok@gmail.com>
2024-06-10 13:35:01 +0900

    Feat: ReverseString2

---
+++ algorithm/src/main/java/basic/quiz/ReverseString2.java
@@ -0,0 +1,12 @@
+package basic.quiz;
+
+public class ReverseString2 {
+    public static void main(String[] args) {
+        String str = "ABCDefg";
+        // 문자 배열로 변환한 뒤 역순으로 출력한다.
+        char chars[] = str.toCharArray();
+        for(int i= chars.length-1; i >= 0; i--) {
+            System.out.print(chars[i]);
+        }
+    }
+}
