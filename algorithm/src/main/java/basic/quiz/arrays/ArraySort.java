f3afb5241be1edaf847a15c7394289988d029029
o3ojunseok <o3ojunseok@gmail.com>
2024-06-11 15:25:33 +0900

    Feat: ArraySort

---
+++ algorithm/src/main/java/basic/quiz/arrays/ArraySort.java
@@ -0,0 +1,23 @@
+package basic.quiz.arrays;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ArraySort {
+    public static void main(String[] args) {
+        Integer[] myArray = { 5, 2, 7, 3, 9 };
+        Arrays.sort(myArray);
+
+        System.out.println(Arrays.toString(myArray));
+
+
+        List myList = Arrays.asList(myArray);
+        Collections.reverse(myList);
+
+        //myList.toArray(myArray);
+
+
+        System.out.println(Arrays.toString(myArray));
+    }
+}
