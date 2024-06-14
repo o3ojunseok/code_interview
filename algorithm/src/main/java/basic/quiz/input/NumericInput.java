4f2476961d7788884927d6e7610593597dc74998
o3ojunseok <o3ojunseok@gmail.com>
2024-06-14 23:33:20 +0900

    Feat: NumericInput & TextInput

---
+++ algorithm/src/main/java/basic/quiz/input/NumericInput.java
@@ -0,0 +1,17 @@
+package basic.quiz.input;
+
+class NumericInput extends TextInput {
+    static String numberInput = "";
+
+    @Override
+    public void add(char c) {
+        if (Character.isDigit(c)){
+            numberInput += c;
+        }
+    }
+
+    @Override
+    public String getValue() {
+        return numberInput;
+    }
+}
---
+++ algorithm/src/main/java/basic/quiz/input/TextInput.java
@@ -0,0 +1,12 @@
+package basic.quiz.input;
+
+class TextInput {
+    static String textInput = "";
+    public void add(char c) {
+        textInput += c;
+    }
+
+    public String getValue() {
+        return textInput;
+    }
+}
