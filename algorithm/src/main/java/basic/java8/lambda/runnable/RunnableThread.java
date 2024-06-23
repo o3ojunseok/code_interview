ecfed6a2b76ac0e592e402474d616e0353f3a3bd
o3ojunseok <o3ojunseok@gmail.com>
2024-06-23 20:31:12 +0900

    Feat: RunnableLambda

---
+++ algorithm/src/main/java/basic/java8/lambda/runnable/RunnableThread.java
@@ -0,0 +1,8 @@
+package basic.java8.lambda.runnable;
+
+public class RunnableThread implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Runnable 구현 " + Thread.currentThread().getName());
+    }
+}
