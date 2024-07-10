5e72657d1f085b1a662f0e4b514f28083b5b990f
o3ojunseok <o3ojunseok@gmail.com>
2024-07-10 14:30:12 +0900

    Feat: FacadeApp

---
+++ algorithm/src/main/java/basic/pattern/facade/FacadeApp.java
@@ -0,0 +1,11 @@
+package basic.pattern.facade;
+
+public class FacadeApp {
+    public static void main(String[] args) {
+        String tableName="Company";
+        //generating MySql HTML report and Oracle PDF report using Facade
+        HelperFacade.generateReport(HelperFacade.DBTypes.MYSQL, HelperFacade.ReportTypes.HTML, tableName);
+        HelperFacade.generateReport(HelperFacade.DBTypes.ORACLE, HelperFacade.ReportTypes.PDF, tableName);
+
+    }
+}
